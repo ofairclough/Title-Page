c171b5476213f8ccff1ebf8241581f6f9821975b
ofairclough <ofairclough@gmail.com>
2014-12-17 01:50:05 -0500

    my first upload

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    
+   $('div').mouseenter(function() {
+   	$(this).fadeTo('fast',0.75);
+});
+   	$('div').mouseleave(function() {
+   	$(this).fadeTo('fast',1);
+
+   	});
+
+
+  });
+
