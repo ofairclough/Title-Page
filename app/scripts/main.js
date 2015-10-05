a604ca1e17419aaa916002d99d336acb4d50a596
ofairclough <ofairclough@gmail.com>
2015-10-05 10:16:06 -0400

    weste

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
+
+
+
+
+var url= window.location.href.split('/')
+
+ if( $.inArray('western', url) > -1){
+
+$(".navbar-brand").html('Western Canada and Territories ISB') 
+
+$(".esp,.local").css('display','none')
+ }
+
+
     
    $('.tile').mouseenter(function() {
    	$(this).fadeTo('fast',0.75)
